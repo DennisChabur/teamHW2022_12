6bf8278c88571951d914adcbab8aeafd3fa200dd
Den <workplacel@mail.ru>
2023-01-05 03:05:27 +0500

    first version of inputs.py and outputs.py

--- PeoplesBase/outputs.py
+++ PeoplesBase/outputs.py
@@ -1,10 +1,33 @@
-def print_menu(user):
-    return
+import csv
+from menu import admin, pupil, teacher
+from inputs import pass
 
 
-def print_array(array):
-    return
+
+def print_menu(user_type):
+    if user_type == 1:
+        return admin
+    elif user_type == 2:
+        return pupil
+    elif user_type == 3:
+        return teacher
+
+def print_array(array, user, user_type):
+    with open(source_path, 'r', encoding='utf-8') as csvfile:
+        file_reader = csv.reader(csvfile, delimiter=';', skipinitialspace=False)
+        info = []
+        for row in file_reader:
+            if user in row:
+                info.append(row)
+    return info
+
 
 
 def print_aray_search(array, search_string):
-    return
+    with open(source_path, 'r', encoding='utf-8') as csvfile:
+        file_reader = csv.reader(csvfile, delimiter=';', skipinitialspace=False)
+        info = []
+        for row in file_reader:
+            if search_string in row:
+                info.append(row)
+    return info
